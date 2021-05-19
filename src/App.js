0f9acd0c70bb18bc2c556225cccabc9a970787a8
limin <limin.shen@smartnews.com>
2021-05-19 11:06:49 +0800

    change file

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React !!!!
+          Learn React !!!!999999
         </a>
       </header>
     </div>
